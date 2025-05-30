ba526d8c265f4eb99bd99390078ad6186da655b7
ChhitizV <chhitizvaidya10@gmail.com>
2025-05-30 21:58:31 +0545

    OSI Layer 7

--- OSI-Model.py
+++ OSI-Model.py
@@ -1,1 +1,9 @@
 # OSI Layer Explorer - CLI Version
+
+osi_layers = [
+    {
+        'layer': 'Layer 7 - Application',
+        'description': 'The layer users interact with. Examples: HTTP, FTP, DNS, POP3',
+        'protocols': ['HTTP', 'HTTPS', 'FTP', 'DNS', 'POP3']
+    },
+]
